8a580fd59bbc80512c547d17f916bc9897fee535
Marcos Vinícius Rodrigues Pires de Lima <mvrpl@icloud.com>
2020-04-23 16:11:07 -0300

    Update main.scala

--- app/src/main/scala/main.scala
+++ app/src/main/scala/main.scala
@@ -3,6 +3,7 @@ import akka.http.scaladsl.Http
 import akka.http.scaladsl.server.Directives._
 import akka.stream.ActorMaterializer
 import org.apache.spark.sql.SparkSession
+import com.typesafe.config.ConfigFactory
 
 object WebServer extends App {
 
@@ -17,21 +18,25 @@ object WebServer extends App {
 	val locale = new java.util.Locale("pt", "BR")
 	val formatter = java.text.NumberFormat.getIntegerInstance(locale)
 
+	val config = ConfigFactory.load()
+	val host = config.getString("http.host")
+	val port = config.getInt("http.port")
+
 	val route =
 		path("wc") {
 			post {
 				formFields('busca.as[String]) { (busca) =>
 					val tempoInicial = System.nanoTime
-					val resultado = formatter.format(sc.textFile("/home/mvrpl/app/input/*.txt").map(line => line.split(" ").count(_ == busca)).reduce(_ + _))
+					val resultado = formatter.format(sc.textFile("/home/marcos/*.txt").map(line => line.split(" ").count(_ == busca)).reduce(_ + _))
 					val duracao = (System.nanoTime - tempoInicial) / 1e9d
 					complete(s"O termo '${busca}' apareceu ${resultado}x\nA pesquisa demorou ${duracao} segundos\n")
 				}
 			}
 		}
 
-	val bindingFuture = Http().bindAndHandle(route, "0.0.0.0", 5001)
+	val bindingFuture = Http().bindAndHandle(route, host, port)
 
-	println(s"Servidor online em 0.0.0.0:5001\nPressione ctrl+c para encerrar...")
+	println(s"Servidor online em ${host}:${port}\nPressione ctrl+c para encerrar...")
 
 	sys.addShutdownHook({
 		println("\nDesligando WebServer...")
